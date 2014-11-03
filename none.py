f61fc91187105c79fd54cbd144cbd34db3a6c291
scivision <scivision@users.noreply.github.com>
2014-11-03 11:58:52 -0500

    showing that Numba and Python handle NaN better than None

--- none.py
+++ none.py
@@ -1,16 +1,35 @@
 #!/usr/bin/env python3
 from numba import jit
 from numba import __version__ as numbavers
+import numpy as np
 ''' shows that Numba 0.15.1 doesn't understand "is not None" '''
 
-@jit
+''' uncomment @jit to show error'''
+#@jit
 def nonetest(x=None):
+    ''' In Numba 0.15.1, this is known to give error:
+    numba.lowering.LoweringError: Failed at object mode backend
+    Internal error:
+    ValueError: 'is not' is not in list
+    '''
     if x is not None:
         print(x)
     else:
         print('x was None')
 
+#@jit
+def nantest(x=np.nan):
+    if np.isnan(x):
+        print('x is NaN')
+    else:
+        print(x)
+
 if __name__ == '__main__':
     print('Numba version ' + str(numbavers))
     nonetest()
     nonetest(3)
+
+    nantest()
+    nantest(float('nan'))
+    #nantest(None) #this isn't possible even without Numba--typeError
+    nantest(3)
