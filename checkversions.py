ec9114f0a06bf1659f7dbb0d6eb58bb6eefbb8c2
scienceopen <scienceopen@users.noreply.github.com>
2014-11-12 18:49:57 -0500

    better dir handling

--- checkversions.py
+++ checkversions.py
@@ -1,17 +1,23 @@
 # -*- coding: utf-8 -*-
 import importlib
 import platform as p
-modules=('numpy','scipy','astropy','pandas','h5py','matplotlib','cython','numba','six')
+modules=('numpy','scipy','astropy','pandas','h5py','matplotlib','cython','numba','six','xlrd')
+from os.path import join
 
-with open('vers.log','w') as f:
-    #first let's log system info
-    f.write('\n'.join((p.node(),p.processor(),p.system(),p.platform())) + '\n' +
-               ' '.join((p.python_implementation(), p.python_version())) + '\n')
-    #now we'll log module versions
-    for mod in modules:
-        try:
-            m = importlib.import_module(mod)
-            #print(mod,m.__version__)
-            f.write(' '.join((mod, m.__version__)) + '\n')
-        except ImportError:
-            f.write(mod + ' not installed\n')
+def main(odir):
+    with open(join(odir,'vers.log'),'w') as f:
+        #first let's log system info
+        f.write('\n'.join((p.node(),p.processor(),p.system(),p.platform())) + '\n' +
+                   ' '.join((p.python_implementation(), p.python_version())) + '\n')
+        #now we'll log module versions
+        for mod in modules:
+            try:
+                m = importlib.import_module(mod)
+                #print(mod,m.__version__)
+                f.write(' '.join((mod, m.__version__)) + '\n')
+            except ImportError:
+                f.write(mod + ' not installed\n')
+
+if __name__=='__main__':
+    from sys import argv
+    main(argv[1])
