d389e78530589810da8e8f68201a5e1221c8ee29
scivision <scivision@users.noreply.github.com>
2014-11-12 15:49:17 -0500

    initial commit

---
+++ checkversions.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+import importlib
+import platform as p
+modules=('numpy','scipy','astropy','pandas','h5py','matplotlib','cython','numba','six')
+
+with open('vers.log','w') as f:
+    #first let's log system info
+    f.write('\n'.join((p.node(),p.processor(),p.system(),p.platform())) + '\n' +
+               ' '.join((p.python_implementation(), p.python_version())) + '\n')
+    #now we'll log module versions
+    for mod in modules:
+        try:
+            m = importlib.import_module(mod)
+            #print(mod,m.__version__)
+            f.write(' '.join((mod, m.__version__)) + '\n')
+        except ImportError:
+            f.write(mod + ' not installed\n')
