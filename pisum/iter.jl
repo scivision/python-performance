7701478ddad465cbb3fd5224af293e451f4b08bb
scivision <scivision@users.noreply.github.com>
2016-11-15 12:00:39 -0500

    Pull Request from @s-broda

--- pisum/iter.jl
+++ pisum/iter.jl
@@ -1,4 +1,4 @@
 #!/usr/bin/env julia
 
-function f(n) x = 0.; for ii = 0.:n-1.; x = 0.5*x + mod(ii,10.); if x>1e100; break; end; end; end
-@time f(1e6)
+function f(n) x = 0.; for ii = 0.:n-1.; x = 0.5*x + mod(ii,10.); if x>1e100; break; end; end; return x; end
+ @time f(10^6)
