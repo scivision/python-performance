34e0b39782c54376e8740df1bbb57f280a44e743
scivision <scivision@users.noreply.github.com>
2016-12-08 03:55:55 -0500

    fixup matlab pisum

--- pisum/iter.m
+++ pisum/iter.m
@@ -1,13 +1,37 @@
-function benchmark_iter()
+function iter()
+
+if isoctave
+    disp('--> Octave')
+else
+    disp('--> Matlab')
+end
+
 %% simple_iter
-%A=rand(1000000,1);
-%f = @() simple_iter(A); % this anon function required by timeit
-%timeit(f)
+  printf('simple_iter ')
+  A = rand(1000000,1);
+  f = @() simple_iter(A); % this anon function required by timeit
+  try
+    timeit(f)
+  catch
+    tic
+    simple_iter(A);
+    t = toc;
+    disp([num2str(t),' sec.'])
+  end
 %% mandelbrot
 %mandel(complex(-.53,.68));
 %assert(sum(sum(mandelperf(true))) == 14791)
-f = @() mandelperf(false);
-timeit(f)
+
+printf('mandelbrot ')
+  try
+    f = @() mandelperf(false);
+    timeit(f)
+  catch
+    tic
+    mandelperf();
+    t = toc;
+    disp([num2str(t),' sec.'])
+  end
 
 end
 
@@ -15,7 +39,7 @@ function x= simple_iter(A) %must return at least one argument or timeit breaks
  x=0;
     for i = A
         x = 0.5*x + mod(i, 10);
-    end 
+    end
 end
 %----------------------------
 function n = mandel(z)
@@ -33,7 +57,7 @@ function M = mandelperf(~)
     x=-2.0:.1:0.5;
     y=-1:.1:1;
     M=zeros(length(y),length(x));
-    for r=1:size(M,1) 
+    for r=1:size(M,1)
         for c=1:size(M,2)
            M(r,c) = mandel(x(c)+y(r)*1j);
         end
