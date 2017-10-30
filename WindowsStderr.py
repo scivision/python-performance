779514a2ca4f8b9892e6eebda74d6585e29f0095
Michael Hirsch, Ph.D <scivision@users.noreply.github.com>
2017-10-30 00:49:35 -0400

    init [ci skip]

---
+++ WindowsStderr.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+"""
+Some Windows configs don't print *any* stderr messages.
+This function is to help test such configurations.
+"""
+import logging
+
+print('this is a stdout message')
+logging.warning('this is a warning message')
+logging.critical('this is a critical message')
+raise RuntimeError('This is a RuntimeError')
