33f639b5cc8bf3dbe0dea383cbcadcf41b164a9b
Michael Hirsch, Ph.D <scivision@users.noreply.github.com>
2018-01-15 20:23:51 -0500

    init

---
+++ setup.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+install_requires = ['cython','matplotlib','numpy','xarray','netcdf4','numba']
+tests_require = ['nose','coveralls']
+# %%
+from setuptools import setup,find_packages
+
+setup(name='python-performance',
+      packages=find_packages(),
+      author='Michael Hirsch, Ph.D.',
+      url='https://github.com/scivision/python-performance',
+      long_description=open('README.rst').read(),
+      description='Python, C, Fortran, Matlab, Julia, CUDA benchmarks',
+      install_requires=install_requires,
+      tests_require=tests_require,
+      extras_require={'tests':tests_require},
+      python_requires='>=2.7',
+      version = '0.1.0',
+	  )
+
