f2ec4ef2cc4df2ecd5f2140e07e9a54bbd2d09e0
Michael Hirsch, Ph.D <scivision@users.noreply.github.com>
2018-02-19 09:55:03 -0500

    cleanup hypot

---
+++ hypot.f90
@@ -0,0 +1,79 @@
+Program run_hypot
+
+  use, intrinsic :: iso_fortran_env, only : dp=>REAL64,INT64
+  use perf, only : init_random_seed,sysclock2ms
+  Implicit None
+
+  integer, parameter :: wp=dp
+  integer :: argc, N, Nrun
+  character(16) :: argv
+  real(wp) :: Rhypot
+
+  argc = command_argument_count()
+  if (argc>0) then
+    call get_command_argument(1,argv)
+    read(argv,*) N
+  else 
+    N = 1000
+  endif
+
+  if (argc>1) then
+    call get_command_argument(2,argv)
+    read(argv,*) Nrun
+  else
+    Nrun = 10
+  endif
+
+  Rhypot = benchhypot(N,Nrun)
+
+  print '(A,F7.5)', 'sqrt(a^2+b^2) / hypot(a,b)  time ratio: ',Rhypot
+
+contains
+
+real(wp) function benchhypot(N,Nrun)
+  integer :: i
+  integer(int64) :: tic,toc,tmin1,tmin2
+
+  integer,intent(in) :: N, Nrun
+  real(wp) :: A(N), B(N), Rhypot(Nrun), Thypot,Trsq
+  ! volatile tells compiler that value is unpredictable, don't unroll, etc.
+  real(wp),volatile :: C(N),D(N)
+
+  tmin1=huge(0_int64)
+  tmin2=huge(0_int64)
+
+  call init_random_seed()
+
+
+! priming
+  call system_clock(tic)
+    C = hypot(A,B)
+  call system_clock(toc)
+
+  main: do i = 1, Nrun
+    call random_number(A)
+    call random_number(B)
+
+    call system_clock(tic)
+      C = hypot(A,B)
+    call system_clock(toc)
+    if (toc-tic<tmin1) tmin1=toc-tic
+
+    Thypot = sysclock2ms(tmin1)
+!-----------
+    call system_clock(tic)
+      D = sqrt(A**2 + B**2)
+    call system_clock(toc)
+    if (toc-tic<tmin2) tmin2=toc-tic
+
+    Trsq = sysclock2ms(tmin2)
+
+    Rhypot(i) = Trsq/Thypot
+
+  enddo main
+
+  benchhypot = sum(Rhypot)/real(Nrun, wp)
+
+end function benchhypot
+
+End Program
