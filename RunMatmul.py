d81fb02ced018e3c69bdd6bf6a32b072b876b86e
Michael Hirsch, Ph.D <scivision@users.noreply.github.com>
2018-02-19 10:41:32 -0500

    cleanup matmul

--- RunMatmul.py
+++ RunMatmul.py
@@ -1,8 +1,7 @@
 #!/usr/bin/env python
 from __future__ import print_function
-from time import time
 import logging
-import subprocess as S
+import subprocess
 from six import PY2
 
 if PY2:
@@ -16,14 +15,14 @@ def test_matmul(juliapath):
     #%% Fortran
     try:
         print ('Fortran -->')
-        S.call(['./bin/matmul'])
+        subprocess.call(['./bin/matmul'])
     except FileNotFoundError:
         logging.error('Fortran test skipped')
         
     #%% Julia
     try:
         print('Julia -->')
-        S.check_call([juliapath+'julia','matmul.jl'], cwd=bdir)
+        subprocess.check_call([juliapath+'julia','matmul.jl'], cwd=bdir)
     except FileNotFoundError:
         logging.warning('Julia executable not found')
 
@@ -31,32 +30,32 @@ def test_matmul(juliapath):
     #%% GDL
     try:
         print('\n --> GDL')
-        S.check_call(['gdl','--version'])
+        subprocess.check_call(['gdl','--version'])
 
-        S.check_call(['gdl','-q','-e','matmul'], cwd=bdir)
+        subprocess.check_call(['gdl','-q','-e','matmul'], cwd=bdir)
     except FileNotFoundError:
         logging.warning('GDL executable not found')
 
     #%% IDL
     try:
         print('\n --> IDL')
-        S.check_call(['idl','--version'])
+        subprocess.check_call(['idl','--version'])
 
-        S.check_call(['idl','-q','-e','matmul'],cwd=bdir)
+        subprocess.check_call(['idl','-q','-e','matmul'],cwd=bdir)
     except FileNotFoundError:
         logging.warning('IDL executable not found')
 
     #%% Octave
     try:
         print()
-        S.check_call(['octave-cli','-q','matmul.m'], cwd=bdir)
+        subprocess.check_call(['octave-cli','-q','matmul.m'], cwd=bdir)
     except FileNotFoundError:
         logging.warning('Octave executable not found')
         
     #%% Matlab
     try:
         print()
-        S.check_call(['matlab','-nodesktop','-nojvm','-nosplash','-r','matmul; exit'], cwd=bdir)
+        subprocess.check_call(['matlab','-nodesktop','-nojvm','-nosplash','-r','matmul; exit'], cwd=bdir)
     except FileNotFoundError:
         logging.warning('Matlab executable not found')
         
@@ -64,7 +63,7 @@ def test_matmul(juliapath):
     #%% Python
     try:
         print()
-        S.check_call(['ipython','matmul.ipy'], cwd=bdir)
+        subprocess.check_call(['ipython','matmul.ipy'], cwd=bdir)
     except FileNotFoundError:
         logging.error('Python test skipped')
 
--- matmul/matmul.f90
+++ matmul/matmul.f90
@@ -1,52 +1,74 @@
-module benchmark_matmul
-! https://software.intel.com/en-us/mkl-developer-reference-fortran-fortran-95-interface-conventions-for-blas-routines
-! https://software.intel.com/en-us/mkl-developer-reference-fortran-gemm
+Program run_matmul
 
-    use, intrinsic :: iso_fortran_env, only : sp=>REAL32,dp=>REAL64,INT64, stdout=>output_unit
-    use perf, only : init_random_seed, sysclock2ms 
-    Implicit None
-    
+  use, intrinsic :: iso_fortran_env, only : sp=>REAL32,dp=>REAL64, INT64, stdout=>output_unit
+  use perf, only : init_random_seed, sysclock2ms
+
+  Implicit None
+
+  integer,parameter :: wp=dp
+  integer :: N, Nrun, argc
+  character(16) :: argv
+  real(wp) :: tdmatmul,tsmatmul
+
+  argc = command_argument_count()
+  if (argc>0) then
+    call get_command_argument(1,argv)
+    read(argv,*) N
+  else
+    N = 1000
+  endif
+
+  if (argc>1) then
+    call get_command_argument(2,argv)
+    read(argv,*) Nrun
+  else
+    Nrun = 5
+  endif
+
+  tdmatmul = double_matmul(N,Nrun)
+  tsmatmul = single_matmul(N,Nrun)
+
+  print '(A20,F10.4)', 'double matmul (sec): ',tdmatmul
+  print '(A20,F10.4)', 'single matmul (sec): ',tsmatmul
+
 contains
 
-!TODO upgrade to polymorphic
-
-Real(dp) Function double_matmul(N,Nrun)
+Real(wp) Function double_matmul(N,Nrun)
 
     integer, intent(in) :: N,Nrun
 
     real(dp), dimension(N,N) :: A, B, D
-
     integer :: k
     integer(int64) :: tic,toc,tmin=huge(0_int64)  ! MUST BE _int64!!!
 
-    D(:,:) = 0._dp ! cannot initialize automatic array directly
+    D(:,:) = 0. ! cannot initialize automatic array directly
 
     call init_random_seed()
 ! https://github.com/JuliaLang/julia/blob/master/test/perf/micro/perf.f90
 
     print *,'priming double-prec. matmul loop'
     ! recommended to call once before loop per Intel manual
-    call dgemm('N','N',N,N,N,1_dp,A,N,B,N,0_dp,D,N)
+    call dgemm('N','N',N,N,N,1.,A,N,B,N,0.,D,N)
 
     do k = 1, Nrun
     !refilling arrays with random numbers to be sure a clever compiler doesn't workaround
         call random_number(A)
         call random_number(B)
-        
+
         call system_clock(tic)
-        call dgemm('N','N',N,N,N,1_dp,A,N,B,N,0_dp,D,N) !ifort 14 10% faster than gfortran 5
-        !D = matmul(A,B) 
+        call dgemm('N','N',N,N,N,1.,A,N,B,N,0.,D,N) !ifort 14 10% faster than gfortran 5
+        !D = matmul(A,B)
         call system_clock(toc)
-        
+
         if (toc-tic < tmin) tmin=toc-tic
-       
-        if (mod(k,1) == 0) then 
-            write(stdout,'(F6.1,A1)', advance='no') real(k,dp)/Nrun*100,'% done'
+
+        if (mod(k,1) == 0) then
+            write(stdout,'(F6.1,A1)', advance='no') real(k,wp)/Nrun*100,'% done'
             flush(stdout)
         endif
     end do
 
-    double_matmul=sysclock2ms(tmin)
+    double_matmul = sysclock2ms(tmin)
     print *,''
     print '(A,F10.3)', 'fortran real64 ms / iteration: ', double_matmul
 
@@ -54,7 +76,7 @@ end function double_matmul
 
 
 
-Real(dp) function single_matmul(N,Nrun)
+Real(wp) function single_matmul(N,Nrun)
 
     integer, intent(in) :: N,Nrun
 
@@ -63,7 +85,7 @@ Real(dp) function single_matmul(N,Nrun)
     integer :: k
     integer(int64) :: tic,toc, tmin=huge(0_int64)
 
-    D(:,:) = 0_sp 
+    D(:,:) = 0.
 
     call init_random_seed()
 
@@ -71,31 +93,31 @@ Real(dp) function single_matmul(N,Nrun)
 
     print *,'priming single-prec. matmul loop'
     ! recommended to call once before loop per Intel manual
-    call sgemm('N','N',N,N,N,1_sp,A,N,B,N,0_sp,D,N)
+    call sgemm('N','N',N,N,N,1.,A,N,B,N,0.,D,N)
 
     do k = 1, Nrun
     !refilling arrays with random numbers to be sure a clever compiler doesn't workaround
         call random_number(A)
         call random_number(B)
-        
+
         call system_clock(tic)
         call sgemm('N','N',N,N,N,1_sp,A,N,B,N,0_sp,D,N)  !single prec only
-        !D = matmul(A,B) 
+        !D = matmul(A,B)
         call system_clock(toc)
-        
+
         if (toc-tic < tmin) tmin=toc-tic
-       
-        if (mod(k,1) == 0) then 
-            write(stdout,'(F6.1,A1)', advance='no') real(k,dp)/Nrun*100,'% done'
+
+        if (mod(k,1) == 0) then
+            write(stdout,'(F6.1,A1)', advance='no') real(k,wp)/Nrun*100,'% done'
             flush(stdout)
         endif
     end do
 
-    single_matmul=sysclock2ms(tmin)
+    single_matmul = sysclock2ms(tmin)
 
     print *,''
     print '(A,F10.3)', 'fortran real32 ms / iteration: ', single_matmul
 
 end function single_matmul
 
-end module benchmark_matmul
+End Program
