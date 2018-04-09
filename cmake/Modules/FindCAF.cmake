ab919e5017ea53233662dd36975f1132a7e60817
Michael Hirsch, Ph.D <scivision@users.noreply.github.com>
2018-04-09 15:02:28 -0400

    cmake 3.12 [ci skip]

--- cmake/Modules/FindCAF.cmake
+++ cmake/Modules/FindCAF.cmake
@@ -1,6 +1,6 @@
 # Determines if/where Coarray Fortran libraries are installed (for GNU GFortran >= 2008)
 
-find_path(CAF_DIR libcoarrays HINTS ENV CAF_DIR)
+find_path(CAF_DIR libcoarrays HINTS ENV CAF_ROOT)
 
 FIND_LIBRARY(CAF_LIBRARY  NAMES caf_mpi 
                           PATHS /usr/lib64 /usr/lib /usr/local/lib64 /usr/local/lib
