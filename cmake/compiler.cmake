4401ab970d8756474db6bbfcde2385138c778d5a
Michael Hirsch, Ph.D <scivision@users.noreply.github.com>
2020-03-03 23:16:08 -0500

    march=native

--- cmake/compiler.cmake
+++ cmake/compiler.cmake
@@ -1,15 +1,17 @@
 if(CMAKE_Fortran_COMPILER_ID STREQUAL Intel)
   if(WIN32)
     string(APPEND CMAKE_Fortran_FLAGS " /warn /heap-arrays")
+    add_compile_options(/arch=native)
   else()
     string(APPEND CMAKE_Fortran_FLAGS " -warn -heap-arrays")
+    add_compile_options(-march=native)
   endif()
 
   string(APPEND CMAKE_Fortran_FLAGS_DEBUG " -traceback -debug extended -check all -fpe0")
 elseif(CMAKE_Fortran_COMPILER_ID STREQUAL GNU)
   string(APPEND CMAKE_Fortran_FLAGS " -fimplicit-none")
 
-  add_compile_options(-mtune=native -Wall -Wextra)
+  add_compile_options(-march=native -Wall -Wextra)
 
   string(APPEND CMAKE_Fortran_FLAGS_DEBUG " -ffpe-trap=zero,overflow,underflow")
 
