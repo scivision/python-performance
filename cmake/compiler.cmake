79c5fd7bd4756d692d0d54501bf36324477d3046
Michael Hirsch <scivision@users.noreply.github.com>
2021-10-10 20:21:41 -0400

    CMAKE_BUILD_TYPE env var

--- cmake/compiler.cmake
+++ cmake/compiler.cmake
@@ -1,17 +1,12 @@
-if(CMAKE_Fortran_COMPILER_ID MATCHES Intel)
-  if(WIN32)
-    string(APPEND CMAKE_Fortran_FLAGS " /warn /heap-arrays")
-    add_compile_options(/QxHost)
-  else()
-    string(APPEND CMAKE_Fortran_FLAGS " -warn -heap-arrays")
-    add_compile_options(-xHost)
-  endif()
-
-  string(APPEND CMAKE_Fortran_FLAGS_DEBUG " -traceback -debug extended -check all -fpe0")
+if(CMAKE_Fortran_COMPILER_ID MATCHES "^Intel")
+  add_compile_options(
+  "$<IF:$<BOOL:${WIN32}>,/QxHost,-xHost>"
+  "$<$<COMPILE_LANGUAGE:Fortran>:-warn;-heap-arrays>"
+  "$<$<AND:$<COMPILE_LANGUAGE:Fortran>,$<CONFIG:Debug>>:-traceback;-debug extended;-check all;-fpe0>"
+  )
 elseif(CMAKE_Fortran_COMPILER_ID STREQUAL GNU)
-  string(APPEND CMAKE_Fortran_FLAGS " -fimplicit-none")
-
-  add_compile_options(-march=native -Wall -Wextra)
-
-  string(APPEND CMAKE_Fortran_FLAGS_DEBUG " -ffpe-trap=zero,overflow,underflow")
+  add_compile_options(-march=native -Wall -Wextra
+  "$<$<COMPILE_LANGUAGE:Fortran>:-fimplicit-none>"
+  "$<$<AND:$<COMPILE_LANGUAGE:Fortran>,$<CONFIG:Debug>>:-ffpe-trap=zero,overflow,underflow>"
+  )
 endif()
