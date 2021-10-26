ec210ab2fec0d5ac4d24de2e5538585ba5e69b23
Michael Hirsch <scivision@users.noreply.github.com>
2021-10-26 13:17:46 -0400

    correct flags

--- cmake/compiler.cmake
+++ cmake/compiler.cmake
@@ -1,12 +1,12 @@
 if(CMAKE_Fortran_COMPILER_ID MATCHES "^Intel")
   add_compile_options(
-  "$<IF:$<BOOL:${WIN32}>,/QxHost,-xHost>"
-  "$<$<COMPILE_LANGUAGE:Fortran>:-warn;-heap-arrays>"
-  "$<$<AND:$<COMPILE_LANGUAGE:Fortran>,$<CONFIG:Debug>>:-traceback;-debug extended;-check all;-fpe0>"
+  $<IF:$<BOOL:${WIN32}>,/QxHost,-xHost>
+  $<$<COMPILE_LANGUAGE:Fortran>:-warn;-heap-arrays>
+  $<$<AND:$<COMPILE_LANGUAGE:Fortran>,$<CONFIG:Debug>>:-traceback;-debug;-check;-fpe0>
   )
 elseif(CMAKE_Fortran_COMPILER_ID STREQUAL GNU)
   add_compile_options(-march=native -Wall -Wextra
-  "$<$<COMPILE_LANGUAGE:Fortran>:-fimplicit-none>"
+  $<$<COMPILE_LANGUAGE:Fortran>:-fimplicit-none>
   "$<$<AND:$<COMPILE_LANGUAGE:Fortran>,$<CONFIG:Debug>>:-ffpe-trap=zero,overflow,underflow>"
   )
 endif()
