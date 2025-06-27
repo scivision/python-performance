d2abc02a02e869bbfcd0688211b8a3066d969fdc
Michael Hirsch <scivision@users.noreply.github.com>
2025-06-27 19:32:49 -0400

    cmake: apply only necessary options in default

--- cmake/compiler.cmake
+++ cmake/compiler.cmake
@@ -5,8 +5,9 @@ if(CMAKE_Fortran_COMPILER_ID MATCHES "^Intel")
   "$<$<AND:$<COMPILE_LANGUAGE:Fortran>,$<CONFIG:Debug>>:-traceback;-debug;-check;-fpe0>"
   )
 elseif(CMAKE_Fortran_COMPILER_ID STREQUAL GNU)
-  add_compile_options(-march=native -Wall -Wextra
+  add_compile_options(-march=native
   $<$<COMPILE_LANGUAGE:Fortran>:-fimplicit-none>
+  "$<$<AND:$<COMPILE_LANGUAGE:Fortran>,$<CONFIG:Debug,RelWithDebInfo>>:-Wall;-Wextra>"
   "$<$<AND:$<COMPILE_LANGUAGE:Fortran>,$<CONFIG:Debug>>:-ffpe-trap=zero,overflow,underflow>"
   )
 endif()
